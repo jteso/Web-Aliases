54efafd2f4d95b2e0fd4080ea2de906bec0bb008
Javier Teso <jtejob@gmail.com>
2011-09-23 16:55:47 +1000

    db added

--- routes.rb
+++ routes.rb
@@ -1,10 +1,53 @@
 require 'rubygems'
 require 'sinatra'
 require 'uri'
+require 'dm-core'
+require 'dm-timestamps'
+require 'dm-aggregates'
+require 'dm-migrations'
+
+# --------------------------------------------------
+# Model
+# --------------------------------------------------
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/alias.db")
+
+class Alias
+  include DataMapper::Resource
+  property :id,         Serial
+  property :user,       Text, :required => true
+  property :alias,      Text, :required => true
+  property :url,        Text, :required => true
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
+
+#DataMapper.finalize.auto_migrate!
 
 # --------------------------------------------------
 # Routes
 # --------------------------------------------------
+
+# Quick and dirty solution to insert initial values in DB. 
+# TODO -- Replace this with dm-migrations.   
+
+get '/migrateup' do
+  initial_aliases = basic_aliases
+  initial_aliases.each do |key,value|
+    a = Alias.new
+    a.user       = "jteso"
+    a.alias      = "#{key}"
+    a.url        = "#{value}"
+    a.created_at = Time.now
+    a.updated_at = Time.now
+    a.save
+   # puts "alias #{alias} saved. "
+  end  
+    redirect '/'  
+end
+    
+   
                 
 ['/', '/home'].each do |path|
   get path do
@@ -38,6 +81,24 @@ get '/execute' do
    
 end
 
+get '/add/:alias/:url' do
+  
+    _alias = "#{params[:alias]}"
+    #sanitising url
+    _url   = "#{params[:url]}"
+    _url   = "http://" + _url unless "#{params[:url]}".include? ("http://")
+    
+    a = Alias.new
+    a.user       = "jteso"
+    a.alias      = _alias
+    a.url        = _url
+    a.created_at = Time.now
+    a.updated_at = Time.now
+    a.save
+    erb :add
+end
+
+
 get '/:command' do
   # If required, pass the paramater to the view
   if params[:command] == "alias"
@@ -46,7 +107,7 @@ get '/:command' do
   if params[:command] == "help"
     @commands = basic_commands
   end
-
+    
   erb params[:command].to_sym
 end
 
@@ -58,18 +119,36 @@ end
 def basic_aliases
   basic_aliases = {
                     :g        => 'http://www.google.com/search?q={query}',
+                    :b        => 'http://www.bing.com/search?q={query}',
                     :gl       => 'http://www.google.com/search?btnI=I2+Feeling+Lucky&q={query}',
+                    :gd       => 'https://docs.google.com/?ui%3D1&ltmpl=homepage#search/{query}',
                     :gi       => 'http://images.google.com/search?q={query}&biw=1276&bih=702&tbm=isch',
                     :gm       => 'http://maps.google.com/maps?q={query}',
                     :gt       => 'http://translate.google.com/?text={query}',
                     :gr       => 'http://www.google.com/reader',
-                    :gmail    => 'http://mail.google.com',
-                    :amazon   => 'http://www.amazon.com/s?url=search-alias=aps&field-keywords={query}',
+                    :gml      => 'http://mail.google.com',
+                    :gn       => 'http://www.google.com/search?aq=f&hl=en&gl=au&tbm=nws&btnmeta_news_search=1&q={query}',
+                    :utb      => 'http://www.youtube.com/results?search_query={query}',
+                    :am       => 'http://www.amazon.com/s?url=search-alias=aps&field-keywords={query}',
                     :weather  => 'http://weather.yahoo.com/search/weather?location={query}',
-                    :cba      => 'https://www.my.commbank.com.au/netbank/Logon/Logon.aspx',
-                    :as       => 'http://www.as.com',
-                    :rdoc     => 'http://www.ruby-doc.org/core/classes/{query}.html',
+                    :tw       => 'http://twitter.com',
+                    :twu      => 'http://twitter.com/#!/search/{query}', #search for users
+                    :twt      => 'http://twitter.com/#!/{query}', #search for terms
+                    :wiki     => 'http://en.wikipedia.org/wiki/{query_}',
+                    :imdb     => 'http://www.imdb.com/find?s=all&q={query}',
+                    :rt       => 'http://www.rottentomatoes.com/m/{query_}',
+                    :ebay     => 'http://www.ebay.com/sch/?_nkw={query}',
+                    :lin      => 'http://www.linkedin.com',
+                    :linu     => 'http://www.linkedin.com/commonSearch?type=people&keywords={query}', #search for users
+                    :fb       => 'http://www.facebook.com',
+                    :fbu      => 'http://www.facebook.com/search/?q={query}',
+                    :flkr     => 'http://www.flickr.com/search/?q={query}&w=all'
                   }
+                  
+ #                  :cba      => 'https://www.my.commbank.com.au/netbank/Logon/Logon.aspx',
+ #                  :as       => 'http://www.as.com',
+ #                  :rdoc     => 'http://www.ruby-doc.org/core/classes/{query}.html',
+                  
 end
 
 def basic_commands
@@ -115,6 +194,13 @@ def inject_param(url,param)
      else
         url.gsub('{query}', param)
      end
+  elsif url.include? "{query_}"
+      if param == nil
+        uri_split = URI.split(url.gsub('{query}','')) # making the URI valid
+        uri_split[0] + "://" + uri_split[2] 
+      else
+         url.gsub('{query_}', param.gsub('+','_'))
+      end
   else
     # static alias -- ignore 'param'
     url
