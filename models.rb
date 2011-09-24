e0b2115346ae9acc64c1903d7dce03240ddab769
Javier Teso <jtejob@gmail.com>
2011-09-25 01:06:10 +1000

    models properly done

---
+++ models.rb
@@ -0,0 +1,136 @@
+require 'rubygems'
+require 'sinatra'
+require 'dm-core'
+require 'dm-timestamps'
+require 'dm-aggregates'
+require 'dm-migrations'
+require 'uri'
+# --------------------------------------------------
+# Model
+# --------------------------------------------------
+DataMapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/alias.db")
+class Command
+  include DataMapper::Resource
+  property :id,     Serial
+  property :name,   Text
+  property :desc,   Text
+  property :url,    Text
+  
+  def self.basic_commands
+    basic_commands = {
+                       ':help'  => {:desc => 'Show a list of all available commands',  :url=>'/help'},
+                       ':date'  => {:desc => 'Display Date and Time',                  :url=>'/date'},
+                       ':alias' => {:desc => 'Show all aliases available',             :url=>'/alias'}
+                      }
+  end
+  
+  def self.url(p_name)
+     record = first(:name => p_name)
+     record ? record.url : nil
+  end
+end
+
+
+class Alias
+  include DataMapper::Resource
+  property :id,         Serial
+  property :user,       Text, :required => true
+  property :alias,      Text, :required => true
+  property :url,        Text, :required => true
+  property :desc,       Text, :default => 'Not Available'
+  property :created_at, DateTime
+  property :updated_at, DateTime
+  
+  def self.basic_aliases
+    basic_aliases = {
+                       :g        => {:desc => 'Search Google for {query}',      :url=>'http://www.google.com/search?q={query}'},
+                       :b        => {:desc => 'Search Bing for {query}',        :url=>'http://www.bing.com/search?q={query}'},
+                       :y        => {:desc => 'Search Yahoo! for {query}',      :url=>'search.yahoo.com/search?p={query}'},
+                       :gl       => {:desc => 'I am feeling lucky for {query}', :url=>'http://www.google.com/search?btnI=I2+Feeling+Lucky&q={query}'},
+                       :gd       => {:desc => 'Open Google Docs or Search Google Docs for {query}', :url=>'https://docs.google.com/?ui%3D1&ltmpl=homepage#search/{query}'},
+                       :gi       => {:desc => 'Search Google Images for {query}',:url=>'http://images.google.com/search?q={query}&biw=1276&bih=702&tbm=isch'},
+                       :gm       => {:desc => 'Search Google Maps for {query}', :url=>'http://maps.google.com/maps?q={query}'},
+                       :gt       => {:desc => 'Translate {query}',              :url=>'http://translate.google.com/?text={query}'},
+                       :gr       => {:desc => 'Open Google Reader',             :url=>'http://www.google.com/reader'},
+                       :gml      => {:desc => 'Open GMail',                     :url=>'http://mail.google.com'},
+                       :gn       => {:desc => 'Open Google News or Search News for {query}', :url=>'http://www.google.com/search?aq=f&hl=en&gl=au&tbm=nws&btnmeta_news_search=1&q={query}'},
+                       :utb      => {:desc => 'Open Youtube or Search Videos for {query}',   :url=>'http://www.youtube.com/results?search_query={query}'},
+                       :am       => {:desc => 'Open Amazon or Search Books for {query}',     :url=>'http://www.amazon.com/s?url=search-alias=aps&field-keywords={query}'},
+                       :weather  => {:desc => 'Weather for {query}',            :url=>'http://weather.yahoo.com/search/weather?location={query}'},
+                       :tw       => {:desc => 'Open Twitter or search users for {query}', :url=>'http://twitter.com/#!/{query}'},                 
+                       :twt      => {:desc => 'Search Twitter for {query}',     :url=>'http://twitter.com/#!/search/{query}'}, 
+                       :wiki     => {:desc => 'Search Wikipedia for {query}',   :url=>'http://en.wikipedia.org/wiki/{query_}'},
+                       :imdb     => {:desc => 'Search IMDB  for {query}',       :url=>'http://www.imdb.com/find?s=all&q={query}'},
+                       :rt       => {:desc => 'Search Rotten Tomatoes for {query}', :url=>'http://www.rottentomatoes.com/m/{query_}'},
+                       :ebay     => {:desc => 'Search Ebay for {query}',        :url=>'http://www.ebay.com/sch/?_nkw={query}'},
+                       :lin      => {:desc => 'Open Linkedin or search users for {query}', :url=>'http://www.linkedin.com'},
+                       :fb       => {:desc => 'Open Facebook or search users for {query}', :url=>'http://www.facebook.com/search/?q={query}'},
+                       :flkr     => {:desc => 'Open Flickr or search images for {query}',  :url=>'http://www.flickr.com/search/?q={query}&w=all'}
+                     }
+                     #                  :cba      => 'https://www.my.commbank.com.au/netbank/Logon/Logon.aspx',
+                     #                  :as       => 'http://www.as.com',
+                     #                  :rdoc     => 'http://www.ruby-doc.org/core/classes/{query}.html',
+  end
+  
+  def self.alias?(p_alias)
+    first(:alias => p_alias)? true : false
+  end
+  
+  def self.url(p_alias)
+    record = first(:alias => p_alias)
+    record ? record.url : nil
+  end
+  
+  def self.fallback_url(param, params)
+    fallback_url = 'http://www.google.com/search?q={query}'
+    query = param
+    if params.length > 1
+      query = query + "+" + params.join("+")
+    else
+      query = query + "+" + params[0]
+    end
+    
+    query = URI.escape(query, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    inject_query(fallback_url,query.split)
+    
+  end 
+  
+  def self.inject_query(url, queries)
+     if queries.length == 0
+        remove_param_from_url(url,"{query}","{query_}")
+     else
+        if url.include? "{query}"
+          url.gsub('{query}', build_query_to_replace("+",queries))
+        elsif url.include? "{query_}"
+          url.gsub('{query_}', build_query_to_replace("_",queries))
+        else
+          puts "URL= #{url} does not contain any known match for parameters." 
+          "http://www.google.com"
+        end      
+      end
+  end
+  
+  def self.build_query_to_replace(separator, queries)
+    if queries.length > 1
+      queries.join(separator)
+    else
+      queries[0]
+    end
+    
+  end
+  
+  
+  def self.remove_param_from_url(url, *matches)
+     matches.each do |match|
+       puts "match=#{match}"
+       url = url.gsub(match,'') # making the URI valid
+     end
+     uri_split = URI.split(url) 
+     uri_split[0] + "://" + uri_split[2] # protocol + domain
+  end
+  
+end
+
+DataMapper.finalize.auto_upgrade!
+
+#DataMapper.finalize.auto_migrate!
--- routes.rb
+++ routes.rb
@@ -6,27 +6,10 @@ require 'dm-timestamps'
 require 'dm-aggregates'
 require 'dm-migrations'
 require './partialsupport.rb'
+require './models.rb'
 
 helpers Sinatra::Partials
 
-# --------------------------------------------------
-# Model
-# --------------------------------------------------
-DataMapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/alias.db")
-
-class Alias
-  include DataMapper::Resource
-  property :id,         Serial
-  property :user,       Text, :required => true
-  property :alias,      Text, :required => true
-  property :url,        Text, :required => true
-  property :created_at, DateTime
-  property :updated_at, DateTime
-end
-
-DataMapper.finalize.auto_upgrade!
-
-#DataMapper.finalize.auto_migrate!
 
 # --------------------------------------------------
 # Routes
@@ -35,66 +18,60 @@ DataMapper.finalize.auto_upgrade!
 # Quick and dirty solution to insert initial values in DB. 
 # TODO -- Replace this with dm-migrations.   
 
-get '/migrateup' do
-  initial_aliases = basic_aliases
-  initial_aliases.each do |key,value|
-    a = Alias.new
-    a.user       = "jteso"
-    a.alias      = "#{key}"
-    a.url        = "#{value}"
-    a.created_at = Time.now
-    a.updated_at = Time.now
-    a.save
-   # puts "alias #{alias} saved. "
+get '/migrateup' do  
+  #Delete all instances from the repository
+  Alias.destroy
+  Command.destroy
+  
+  #Load up initial aliases & commands
+  Alias.basic_aliases.each do |key,value|
+    Alias.create(:user =>  "jteso", :alias => "#{key}", :url  => "#{value[:url]}",  :desc => "#{value[:desc]}",
+                 :created_at => Time.now, :updated_at => Time.now)
   end  
-    redirect '/'  
+  
+  Command.basic_commands.each do |key,value|
+    Command.create(:name => "#{key}", :desc => "#{value[:desc]}", :url => "#{value[:url]}")
+  end
+  
+  
+  
+  redirect '/'  
 end
     
    
                 
 ['/', '/home'].each do |path|
   get path do
-   # if session[:userid].nil? then erb :login 
-   # else redirect "/#{User.get(session[:userid]).email}"
-   erb :home
+    erb :home
   end
 end
 
 get '/execute' do
-   websearch = "#{params[:websearch]}"
-   params = websearch.split(' ')
-   if params.length == 1
-     if command_translator(websearch)
-        redirect command_translator(websearch)
-     else
-        redirect(url_translator(websearch, nil))
-     end
-   else
-      command = params[0]
-      # build the parameter for command above
-      i = 2
-      query = params[1]
-      while i < params.length do
-        query = query + "+" + params[i]  
-        i +=1
-      end
-      puts "redirect #{command} #{query.strip}"
-      redirect(url_translator(command, query.strip))
-   end
+   command = "#{params[:command]}"
+   tokens = command.split(' ')
+   
+   command_or_alias = tokens[0]
+   params = tokens.slice(1, tokens.length) 
+   puts "command_or_alias= #{command_or_alias}"
+   puts "params = #{params}"
+   redirect universal_translator(command_or_alias,params)
+
    
 end
-
-get '/add/:alias/:url' do
   
+get '/add/:alias/:url/:desc' do
+    
     _alias = "#{params[:alias]}"
     #sanitising url
     _url   = "#{params[:url]}"
     _url   = "http://" + _url unless "#{params[:url]}".include? ("http://")
+    _desc  = "#{params[:desc]}"
     
     a = Alias.new
     a.user       = "jteso"
     a.alias      = _alias
     a.url        = _url
+    a.desc       = _desc
     a.created_at = Time.now
     a.updated_at = Time.now
     a.save
@@ -103,12 +80,12 @@ end
 
 
 get '/:command' do
-  # If required, pass the paramater to the view
+ 
   if params[:command] == "alias"
-    @alias = basic_aliases
-  end
-  if params[:command] == "help"
-    @commands = basic_commands
+    @aliases = Alias.all(:user => 'jteso')
+  
+  elsif params[:command] == "help"
+    @commands = Command.basic_commands
   end
     
   erb params[:command].to_sym
@@ -118,103 +95,33 @@ end
 # --------------------------------------------------
 # Helpers
 # --------------------------------------------------
-
-def basic_aliases
-  basic_aliases = {
-                    :g        => 'http://www.google.com/search?q={query}',
-                    :b        => 'http://www.bing.com/search?q={query}',
-                    :gl       => 'http://www.google.com/search?btnI=I2+Feeling+Lucky&q={query}',
-                    :gd       => 'https://docs.google.com/?ui%3D1&ltmpl=homepage#search/{query}',
-                    :gi       => 'http://images.google.com/search?q={query}&biw=1276&bih=702&tbm=isch',
-                    :gm       => 'http://maps.google.com/maps?q={query}',
-                    :gt       => 'http://translate.google.com/?text={query}',
-                    :gr       => 'http://www.google.com/reader',
-                    :gml      => 'http://mail.google.com',
-                    :gn       => 'http://www.google.com/search?aq=f&hl=en&gl=au&tbm=nws&btnmeta_news_search=1&q={query}',
-                    :utb      => 'http://www.youtube.com/results?search_query={query}',
-                    :am       => 'http://www.amazon.com/s?url=search-alias=aps&field-keywords={query}',
-                    :weather  => 'http://weather.yahoo.com/search/weather?location={query}',
-                    :tw       => 'http://twitter.com',
-                    :twt      => 'http://twitter.com/#!/search/{query}', #search for users
-                    :twu      => 'http://twitter.com/#!/{query}', #search for terms
-                    :wiki     => 'http://en.wikipedia.org/wiki/{query_}',
-                    :imdb     => 'http://www.imdb.com/find?s=all&q={query}',
-                    :rt       => 'http://www.rottentomatoes.com/m/{query_}',
-                    :ebay     => 'http://www.ebay.com/sch/?_nkw={query}',
-                    :lin      => 'http://www.linkedin.com',
-                    :linu     => 'http://www.linkedin.com/commonSearch?type=people&keywords={query}', #search for users
-                    :fb       => 'http://www.facebook.com',
-                    :fbu      => 'http://www.facebook.com/search/?q={query}',
-                    :flkr     => 'http://www.flickr.com/search/?q={query}&w=all'
-                  }
-                  
- #                  :cba      => 'https://www.my.commbank.com.au/netbank/Logon/Logon.aspx',
- #                  :as       => 'http://www.as.com',
- #                  :rdoc     => 'http://www.ruby-doc.org/core/classes/{query}.html',
-                  
-end
-
-def basic_commands
-  basic_commands = {
-                     ':help'  => '/help',
-                     ':date'  => '/date',
-                     ':alias' => '/alias'
-                    }
+def universal_translator(command, params)
+  url = command_translator(command)
+  url ? url : url_translator(command, params)
 end
                   
 
-def command_translator(websearch)
-  commands = basic_commands
-  commands[websearch]
+def command_translator(command)
+
+  Command.url(command)
+ 
 end
 
 
-def url_translator(websearch, query)
-  basic_searches = basic_aliases
+def url_translator(p_alias, params)
+  puts "url translator"
+  url_found = Alias.url(p_alias)
   
-  url_found = basic_searches[websearch.to_sym]
-
   if  url_found == nil
-    if query == nil
-      fallback(websearch)
-    else
-      fallback(websearch + "+" + query) 
-    end
+    Alias.fallback_url(p_alias,params)
   else
-    inject_param(url_found, query)
+    puts "inject_query calling with url_found = #{url_found} and params=#{params}"
+    Alias.inject_query(url_found, params)
   end
   
  
 end
 
 
-def inject_param(url,param)
-  
-  if url.include? "{query}"
-     if param == nil
-       uri_split = URI.split(url.gsub('{query}','')) # making the URI valid
-       uri_split[0] + "://" + uri_split[2] 
-     else
-        url.gsub('{query}', param)
-     end
-  elsif url.include? "{query_}"
-      if param == nil
-        uri_split = URI.split(url.gsub('{query}','')) # making the URI valid
-        uri_split[0] + "://" + uri_split[2] 
-      else
-         url.gsub('{query_}', param.gsub('+','_'))
-      end
-  else
-    # static alias -- ignore 'param'
-    url
-  end
-  
-  
-end
-
-def fallback(websearch)
-  fallback = 'http://www.google.com/search?q={query}'
-  inject_param(fallback,websearch)
-end
 
 
